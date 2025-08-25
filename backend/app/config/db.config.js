5544b34ea663457bec5e5a11110e04bbedeb8151
Arpit Mishra <117110049+Arpit9838@users.noreply.github.com>
2025-08-25 11:39:11 +0530

    Update db.config.js

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,4 +1,4 @@
 
 module.exports = {
-url:"mongodb+srv://Abhishek:dVyvnFFFBY7NeR0O@cluster0.qp5qg2p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+url:"mongodb+srv://<db_username>:<db_password>@cluster0.qp5qg2p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 };
